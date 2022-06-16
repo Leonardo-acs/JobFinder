3ba6f0e9b7bd0cc1d20c41d77201d212e159cf93
Leonardo Carvalho <leoacs2002@hotmail.com>
2022-06-16 13:32:13 -0300

    new: adaptando a aplicação para subir na aws

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
 const routes = require('./Src/routes/routes');
 const fileUplaod = require('express-fileupload');
-const app = express();
+
+// aws
+const path = require('path')
+const http = require('http')
+const port = process.env.PORT || 3000;
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
+
+app.use('/', express.static(path.join(__dirname,'angular')))
+app.use((req,res, next) => {
+    res.sendFile(__dirname, 'angular', 'index.html');
+})
 
 var cors = require('cors');
-
 app.use((req, res, next) =>{
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
@@ -31,7 +43,10 @@ mongoose.connect('mongodb+srv://Leo:PLeTys3EfbymBmrk@cluster0.beuie.mongodb.net/
 app.use(express.json());
 app.use(routes);
 app.use(fileUplaod());
-
+app.use('/', express.static(path.join(__dirname,'front-end')))
+app.use((req,res, next) => {
+    res.sendFile(__dirname, 'front-end', 'index.html');
+})
 app.listen(4000, () => {
     console.log('Conectado');
 });
